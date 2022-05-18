5e631669565f89f986fc01621d3b877982fe78d2
LiceB <alicebianchi2013@gmail.com>
2022-05-18 01:03:46 -0300

    Alterações necessárias para corrigir os erros do CheckPoint2
    Tirei o vetor da instancia do objeto defensoria e arrumei o seu tamanho,
    criei os objetos advogados para poder ter ele nos processos, criei os
    objetos processo de forma manual, inseri todos eles no objeto defen que
    possui o método inserir, encerrei 2 processos com o método encerrar no
    objeto defen, e por último listei todos os processos com o método listar
    no defen.

--- Alice86850/src/br/fiapjuridica/main/Main.java
+++ Alice86850/src/br/fiapjuridica/main/Main.java
@@ -1,38 +1,34 @@
 package br.fiapjuridica.main;
 
-import static javax.swing.JOptionPane.*;
-import static java.lang.Integer.parseInt;
-import static java.lang.Double.parseDouble;
-
+import br.fiapjuridica.advogado.Advogado;
 import br.fiapjuridica.defensoria.Defensoria;
 import br.fiapjuridica.processo.Processo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Defensoria[] defen = new Defensoria[2];		
+		Defensoria defen = new Defensoria(4);
 		
-		int p1 = parseInt(showInputDialog("Código: "));
-		p1 += parseInt(showInputDialog("Data de abertura: "));
-		p1 += parseInt(showInputDialog("Data de encerramento: "));
-		p1 += parseInt(showInputDialog("Nome: "));
+		Advogado advogado1 = new Advogado(5, "Adriana", "Criminal");
+		Advogado advogado2 = new Advogado(7, "Ana");
+		Advogado advogado3 = new Advogado(9, "Maria");
+		Advogado advogado4 = new Advogado(6, "Amanda");
 		
-		int p2 = parseInt(showInputDialog("Código: "));
-		p2 += parseInt(showInputDialog("Data de abertura: "));
-		p2 += parseInt(showInputDialog("Data de encerramento: "));
-		p2 += parseInt(showInputDialog("Nome: "));
+		Processo p1 = new Processo(1, "01/01/2022", "Alice", advogado1);
+		Processo p2 = new Processo(2, "02/01/2022", "Ester", advogado2);
+		Processo p3 = new Processo(3, "03/01/2022", "João", advogado3);
+		Processo p4 = new Processo(4, "04/01/2022", "Naomi", advogado4);
 		
-		int p3 = parseInt(showInputDialog("Código: "));
-		p3 += parseInt(showInputDialog("Data de abertura: "));
-		p3 += parseInt(showInputDialog("Data de encerramento: "));
-		p3 += parseInt(showInputDialog("Nome: "));
+		defen.inserir(p1);
+		defen.inserir(p2);
+		defen.inserir(p3);
+		defen.inserir(p4);
 		
-		int p4 = parseInt(showInputDialog("Código: "));
-		p4 += parseInt(showInputDialog("Data de abertura: "));
-		p4 += parseInt(showInputDialog("Data de encerramento: "));
-		p4 += parseInt(showInputDialog("Nome: "));
+		defen.encerrar(1, "01/02/2022");
+		defen.encerrar(2, "05/03/2022");
 		
-		
+		System.out.println(defen.listar());
+				
 	}
 
 }
