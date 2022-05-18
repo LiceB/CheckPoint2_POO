19b2c0a04ec1b725acd685d293692d7c8e93a197
LiceB <alicebianchi2013@gmail.com>
2022-05-18 00:54:07 -0300

    Alterações necessárias para corrigir os erros do CheckPoint2
    Deixei o construtor só com a instancia da lista, para encerrar só
    precisso percorrer toda a lista e fazer o teste se o código daquele
    indice é igual ao código informado, e se for verdade então define/set a
    data de encerramento informardo, e no listar só percorri todos os
    indices da lista e armazenei os dados pelo getDados na variável aux

--- Alice86850/src/br/fiapjuridica/defensoria/Defensoria.java
+++ Alice86850/src/br/fiapjuridica/defensoria/Defensoria.java
@@ -6,12 +6,9 @@ public class Defensoria {
 
 	private Processo[] lista;
 	private int indice;
-	Processo processo = new Processo(null, null, null, null);
 
 	public Defensoria(int tamanho) {
-		super();
-		tamanho = tamanho;
-		lista = new Processo[indice];
+		lista = new Processo[tamanho];
 	}
 
 	public void inserir(Processo processo) {
@@ -22,60 +19,36 @@ public class Defensoria {
 	}
 
 	public void encerrar(int codigo, String DataEncerramento) {
-		Processo aux = null;
-
-		for (int i = 0; i < indice; i++) {
+		for (int i = 0; i < lista.length; i++) {
 			if (lista[i].getCodigo() == codigo) {
-				aux = lista[i];
+				lista[i].setDataEncerramento(DataEncerramento);
 			}
-			
-			if(lista[i].getDataEncerramento() == null) {
-				DataEncerramento = DataEncerramento;
-			} else
-				break;
 		}
-		
+
 	}
-	
+
 	public String listar() {
-		String processosA = "Processos Abertos\n";
-		String processosE = "Processos Encerrados\n";
-		
-		for(int i = 0; i < indice; i++) {
-			if(lista[i] instanceof Processo) {
-				processosA += lista[i] + "\n";
-			} else {
-				processosE += lista[i] + "\n";
-			}
+		String aux = "";
+		for (int i = 0; i < lista.length; i++) {
+			aux += lista[i].getDados();
 		}
-		
-		return processosA+"\n"+processosE;
+		return aux;
 	}
 
 	public Processo[] getLista() {
 		return lista;
 	}
 
-	public void setLista(Processo[] lista) {
-		this.lista = lista;
-	}
-
 	public int getIndice() {
 		return indice;
+	}
+
+	public void setLista(Processo[] lista) {
+		this.lista = lista;
 	}
 
 	public void setIndice(int indice) {
 		this.indice = indice;
 	}
-
-	public Processo getProcesso() {
-		return processo;
-	}
-
-	public void setProcesso(Processo processo) {
-		this.processo = processo;
-	}
-	
-	
 
 }
